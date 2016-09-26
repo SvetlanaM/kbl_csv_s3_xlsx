c3f3816c84713ceaeb1894263e6f6e365455ef00
Svetlana <SvetlanaM@users.noreply.github.com>
2016-09-26 16:33:09 +0200

    Update code.py

--- code.py
+++ code.py
@@ -38,6 +38,13 @@ if __name__ == '__main__':
     for file in listOfFiles2:
         temp = os.path.splitext(file[17:len(file)])
         print " === uploading " + 'CE_WK'+str(int(time.strftime("%V")))+temp[1] + " ==="
-        conn.upload('CE_WK'+str(int(time.strftime("%V")))+temp[1],open(file,'rb'),data["parameters"]["bucketName"])
-        print " === " + 'CE_WK'+str(int(time.strftime("%V")))+temp[1] + " uploaded ==="
-        print data["parameters"]["fileName"]
+        if data["parameters"]["fileName"] == "":
+            conn.upload('CE_WK'+str(int(time.strftime("%V")))+temp[1],open(file,'rb'),data["parameters"]["bucketName"])
+            print " === " + 'CE_WK'+str(int(time.strftime("%V")))+temp[1] + " uploaded ==="
+        else:
+            conn.upload(data["parameters"]["fileName"] + ".xlsx",open(file,'rb'),data["parameters"]["bucketName"])
+            print " === " + data["parameters"]["fileName"] + ".xlsx" + " uploaded ==="
+            
+            
+        
+        
